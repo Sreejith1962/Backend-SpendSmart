41c3f01e20b699aba2fcf6fb5ccc121b5a821fb2
Sreejith S <sreejithjnv1962@gmail.com>
2025-03-30 18:49:15 +0530

    added migrations

--- backend/app.py
+++ backend/app.py
@@ -17,8 +17,7 @@ import psycopg2
 import openai
 app = Flask(__name__)
 CORS(app)  
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
